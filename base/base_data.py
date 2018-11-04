d08c59a6545a4140643736c23f8b12aae01ceeed
zhiqin.zhang <zhiqin.zhang@wehotelgloabl.com>
2018-11-04 17:29:06 +0800

    学习代码提交

--- base/base_data.py
+++ base/base_data.py
@@ -1,3 +1,5 @@
+
+#基本数据类型
 a, b, c, d = 20, 5.5, True, 4 + 3j
 
 print(type(a), type(b), type(c), type(d))
---
+++ base/base_data_list.py
@@ -0,0 +1,12 @@
+
+#列表
+
+list=["hello",1,True]
+
+print(list[0])
+print(list[:1])
+print(list[1])
+print(type(list[1]))
+
+
+
